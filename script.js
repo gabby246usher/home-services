4fab14f70146447ac9d18f87ec08a0e46a414a86
gabby246usher <116130218+gabby246usher@users.noreply.github.com>
2024-11-22 23:48:11 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 522);
-});
-
-// const cards = document.querySelectorAll(".card");
-// const cardIcons = document.querySelector(".card-icon");
-
-// cards.forEach((element) => {
-//   element.addEventListener("mouseover", () => {
-//     element.style.backgroundColor = "red";
-
-//     cardIcons.forEach((element) => {
-//       element.style.backgroundColor = "black";
-//     });
-//   });
-
-//   element.addEventListener("mouseout", () => {
-//     element.style.backgroundColor = "";
-//   });
-// });
-
-// cards.addEventListener("mouseover", () => {
-//   cards.style.backgroundColor = "red";
-// });
-
-// cards.addEventListener("mouseout", () => {
-//   cards.style.backgroundColor = "";
-// });
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
